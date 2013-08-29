92e0e22ae343bbc87aeb7c59eda1a643345ebb4f
ldx <ldx@nilvec.com>
2013-08-29 15:22:44 +0200

    Move firefox to tag 2.

--- config.h
+++ config.h
@@ -28,8 +28,9 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",           NULL,       NULL,       1 << 7,       True,        -1 },
 	{ "Google-chrome",  NULL,       NULL,       1 << 1,       False,       -1 },
+	{ "Firefox",        NULL,       NULL,       1 << 1,       False,       -1 },
+	{ "Iceweasel",      NULL,       NULL,       1 << 1,       False,       -1 },
 	{ "Google-chrome",  NULL,       "Google+ Hangouts - Google Chrome",       1 << 7,       False,       -1 },
-
 	{ "trayer",         NULL,       NULL,       1 << 8,       False,       -1 },
 	{ "Skype",          NULL,       NULL,       1 << 8,       False,       -1 },
 };
